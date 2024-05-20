2ea059e3bdc484df038beaa4054f3041125297c0
Dhusnic Infant D M <dhusnicinfant@gmail.com>
2024-05-20 21:04:37 +0530

    create add user to mongo DB

--- api/index.js
+++ api/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
+import authRoute from './routes/auth.route.js';
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{console.log("connected to MongoDB");}).catch((err)=>{console.log(err);})
 const app=express();
+
+app.use(express.json());
+
 app.listen(3000,()=>{console.log('Server listening on port 3000')});
 
 
 app.use("/api/user",userRoute);
+app.use("/api/auth",authRoute);
